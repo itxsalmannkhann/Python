4fb0357c9985e37e8d046651f4597669ea0b8a35
Salman Khan <khanhackersalman@gmail.com>
2025-12-01 12:10:48 +0500

    Add 3 user attributes

--- 01_Basics/hello_world.py
+++ 01_Basics/hello_world.py
@@ -4,3 +4,10 @@ def greet(name):
     print("Hello, " + name + "!")
 
 greet("Salman Khan")
+
+user_1 = "Salman Khan"
+user_2 = "Haroon Khan"
+user_3 = "Arslan Khan"
+greet(user_1)
+greet(user_2)
+greet(user_3)
